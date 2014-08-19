70ea6a6f105e004724b86e5b39474cb77c11a995
Sara Williamson <sara.e.williamson@gmail.com>
2014-08-18 22:51:31 -0400

    Add image upload with Paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,8 @@ Rails.application.configure do
 
   #required for Heroku
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin/"
 end
+
+
